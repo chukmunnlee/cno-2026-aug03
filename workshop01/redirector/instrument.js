833c75afb838cf1c7eef436779dd55e1843b80f4
chukmunnlee <chukmunnlee@gmail.com>
2026-08-03 15:41:40 +0800

    workshop01 task02

--- workshop01/redirector/instrument.js
+++ workshop01/redirector/instrument.js
@@ -1,6 +1,9 @@
 import { register } from 'node:module'
 import { pathToFileURL } from 'node:url'
 
+// register the hook
+register('@opentelemetry/instrumentation/hook.mjs', pathToFileURL('./'))
+
 import { NodeSDK } from '@opentelemetry/sdk-node'
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-grpc'
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node'
@@ -13,7 +16,19 @@ import { ATTR_SERVICE_NAME, ATTR_SERVICE_VERSION } from '@opentelemetry/semantic
 import { logger } from '../lib/logger.js'
 import metadata from '../package.json' with { type: 'json' }
 
-import { logger } from "../lib/logger.js";
-
 const OTEL_EXPORTER_OTLP_ENDPOINT = process.env.OTEL_EXPORTER_OTLP_ENDPOINT
 
+// Create the NodeSDK and enable library instrumentation
+const sdk = new NodeSDK({
+   instrumentations: getNodeAutoInstrumentations()
+})
+
+// Stop the SDK
+process.on('SIGTERM', () => {
+   sdk.shutdown()
+   logger.info("Instrumentation stopped")
+})
+
+// Start the SDK
+logger.info("Instrumentation started")
+sdk.start()
--- workshop01/redirector/routes.js
+++ workshop01/redirector/routes.js
@@ -15,50 +15,70 @@ function injectError(errorProb = 0.3) {
 }
 
 const router = Router();
-const tracer = trace.getTracer(metadata.name, metadata.version)
+//const tracer = trace.getTracer(metadata.name, metadata.version)
+const tracer = trace.getTracer('shortener', 'workshop01')
 
 router.get("/health", (_req, res) => res.json({ status: "ok" }));
 
 router.get("/resolve/:code", async (req, res) => {
 	const { code } = req.params;
 
-  try {
-    const cached = await redis.get(`urls:${code}`);
+  tracer.startActiveSpan('Resolve shortner code', { kind: SpanKind.SERVER }, 
+      async (span) => {
+        // start of work
+        try {
+          const cached = await redis.get(`urls:${code}`);
 
-    let originalUrl;
+          // Add attributes
+          span.setAttributes({
+            [ ATTR_URL_PATH ]: '/resolve/:code',
+            'cache.hit': !!cached
+          })
 
-    if (cached) {
-      originalUrl = cached;
-    } else {
+          let originalUrl;
 
-      const result = await db.query(
-        "SELECT original_url FROM urls WHERE short_code = $1",
-        [code],
-      );
+          if (cached) {
+            originalUrl = cached;
+          } else {
 
-      injectError()
+            const result = await db.query(
+              "SELECT original_url FROM urls WHERE short_code = $1",
+              [code],
+            );
+            injectError()
+            if (result.rows.length === 0) {
+              return res.status(404).json({ error: "Short URL not found" });
+            }
+            originalUrl = result.rows[0].original_url;
+            // add more attributes
+            span.setAttributes({
+              [ ATTR_DB_COLLECTION_NAME ]: 'urls',
+              [ ATTR_DB_QUERY_TEXT ]: "SELECT original_url FROM urls WHERE short_code = $1"
+            })
+            await redis.set(`urls:${code}`, originalUrl, { EX: 86400 });
+            // record an event
+            span.addEvent('Cache updated', { code: code })
+          }
 
-      if (result.rows.length === 0) {
-        return res.status(404).json({ error: "Short URL not found" });
-      }
+          await recordVisit(req, code);
+          res.json({ original_url: originalUrl });
+          // mark span as successful
+          span.setStatus(SpanStatusCode.OK)
 
-      originalUrl = result.rows[0].original_url;
+        } catch (err) {
+          // Record the exception event
+          span.recordException(err)
+          // mark the span as error because of the exception
+          span.setStatus(SpanStatusCode.ERROR)
+          logger.error({ err }, "Resolve failed");
+          res.status(500).json({ error: "Internal server error" });
 
-      await redis.set(`urls:${code}`, originalUrl, { EX: 86400 });
-
-    }
-
-    await recordVisit(req, code);
-
-    res.json({ original_url: originalUrl });
-
-  } catch (err) {
-
-    logger.error({ err }, "Resolve failed");
-    res.status(500).json({ error: "Internal server error" });
-
-  } finally {
-  }
+        } finally {  
+          // end the span
+          span.end() 
+        }
+      } // close the span function
+  ) // close the startActiveSpan
 });
 
 async function recordVisit(req, shortCode) {
